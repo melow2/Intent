40474935272fcb2f0e2502e6982401cda10208b1
happiness <melow2@naver.com>
2020-08-03 15:00:53 +0900

    readme

--- README.md
+++ README.md
@@ -5,19 +5,47 @@
 ## Concept
 액티비티 전환시 정보를 전달하는 목적으로 만들어진 장치.
 
-#### 사용방법
-1. 인텐트 객체 생성.
-2. 호출 메소드 이용하여 액티비티 호출.
+1. 명시적 인텐트
+* 호출 대상이 정해져 있는 경우.
+    ** 예) SecondActivity로 명확한 intent의 목적이 명시되어 있을 때.**
 ```
 Intent intent = new Intent(MainActivity.this,SecondActivity.class); // 컨텍스트,호출할 액티비티
 startActivity(intent)
 ```
-
-## 종류
-1. 명시적 인텐트
-    * 호출 대상이 정해져 있는 경우.(*명확하게 호출 대상이 정해져 있는 경우)
-
 2. 암시적 인텐트
-    * 호출 대상이 정해지지 않은 경우.
+* 호출 대상이 정해지지 않은 경우.
     * **예) 다음 작업을 수행할 브라우저를 선택하세요.**
     * MainActivity에서는 website를 열어라고 intent만 던진다.
+    * 호출 대상을 알아서 선택해야하는 문제.
+```
+mBinding.btnDial.setOnClickListener {
+    uri = Uri.parse("tel:01049307013")
+    intent = Intent(Intent.ACTION_DIAL,uri)
+    startActivity(intent)
+}
+
+mBinding.btnCall.setOnClickListener {
+    uri = Uri.parse("tel:01049307013")
+    intent = Intent(Intent.ACTION_CALL,uri)
+    startActivity(intent)
+}
+
+mBinding.btnContact.setOnClickListener {
+    uri = Uri.parse("content://contacts/people/");
+    intent = Intent(Intent.ACTION_VIEW,uri)
+    startActivity(intent)
+}
+
+mBinding.btnWebsite.setOnClickListener {
+    uri = Uri.parse("http://sba.seoul.kr")
+    intent = Intent(Intent.ACTION_VIEW, uri)
+    startActivity(intent)
+}
+
+mBinding.btnSms.setOnClickListener {
+    intent = Intent(Intent.ACTION_VIEW);
+    intent?.putExtra("sms_body", "문자 메시지 입니다.");
+    intent?.setType("vnd.android-dir/mms-sms");
+    startActivity(intent)
+}
+```
--- app/src/main/java/com/khs/intent/MainActivity.kt
+++ app/src/main/java/com/khs/intent/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.khs.intent
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.khs.intent.databinding.ActivityMainBinding
-import com.khs.intent.databinding.ActivitySecondBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val mBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        var uri: Uri?=null
+        var intent:Intent?=null
+
         mBinding.btnGoSecondActivity.setOnClickListener {
-            val intent = Intent(MainActivity@this,SecondActivity::class.java)
+            intent = Intent(MainActivity@this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        mBinding.btnDial.setOnClickListener {
+            uri = Uri.parse("tel:01049307013")
+            intent = Intent(Intent.ACTION_DIAL,uri)
+            startActivity(intent)
+        }
+
+        mBinding.btnCall.setOnClickListener {
+            uri = Uri.parse("tel:01049307013")
+            intent = Intent(Intent.ACTION_CALL,uri)
+            startActivity(intent)
+        }
+
+        mBinding.btnContact.setOnClickListener {
+            uri = Uri.parse("content://contacts/people/");
+            intent = Intent(Intent.ACTION_VIEW,uri)
+            startActivity(intent)
+        }
+
+        mBinding.btnWebsite.setOnClickListener {
+            uri = Uri.parse("http://sba.seoul.kr")
+            intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        mBinding.btnSms.setOnClickListener {
+            intent = Intent(Intent.ACTION_VIEW);
+            intent?.putExtra("sms_body", "문자 메시지 입니다.");
+            intent?.setType("vnd.android-dir/mms-sms");
             startActivity(intent)
         }
     }
